3e75973d05141d46ec39e9cd539933d0dba88da8
shubhank.joshi <shubhankjjoshi10@gmail.com>
2019-07-18 14:39:43 +0530

    updated batch file

--- AutoBuildTest.bat
+++ AutoBuildTest.bat
@@ -1,6 +1,10 @@
 @echo off
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe ConsoleApplication1\ConsoleApplication1.csproj
 ConsoleApplication1\bin\Debug\ConsoleApplication1.exe
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe UnitTestProject1\UnitTestProject1.csproj
+UnitTestProject1\bin\Debug\UnitTestProject1.exe
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\mstest" /testcontainer:UnitTestProject1\bin\debug\UnitTestProject1.dll /resultsfile:UnitTestProject1\results.trx
+
 echo Error code is %errorlevel%
 rem dont display this
 pause
