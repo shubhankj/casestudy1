e75bb6f5c720494385c500a2292e703d1d6f3748
sweta.jha <sweta.jha@philips.com>
2019-07-24 14:23:21 +0530

    Commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void func()
+	{
+		System.out.println("Hello world!");
+	}
+	public static void main(String[] args) {
+		HelloWorld hw=new HelloWorld();
+		hw.func();
+	}
+
+}
