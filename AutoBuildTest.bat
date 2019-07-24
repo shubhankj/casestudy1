7d581f85e4496c1c600ef7a2639399cfa291e0b9
sweta.jha <sweta.jha@philips.com>
2019-07-24 16:02:50 +0530

    Commit

--- AutoBuildTest.bat
+++ AutoBuildTest.bat
@@ -1,10 +1,12 @@
 @echo off
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe ConsoleApplication1\ConsoleApplication1.csproj
-ConsoleApplication1\bin\Debug\ConsoleApplication1.exe
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe UnitTestProject1\UnitTestProject1.csproj
-UnitTestProject1\bin\Debug\UnitTestProject1.exe
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\mstest" /testcontainer:UnitTestProject1\bin\debug\UnitTestProject1.dll /resultsfile:UnitTestProject1\results.trx
+rem ConsoleApplication1\bin\Debug\ConsoleApplication1.exe
+cd spotbugs-3.1.12\bin
+START "spotbugs" spotbugs.bat -textui -maxHeap 1500 -nested:false -output ..\..\ReportFile.emacs -effort:max -low -sortByClass -emacs sourcepath ..\..\HelloWorld
+cd ..\..\pmd-bin-6.16.0\bin
+START .\pmd.bat -d ..\..\HelloWorld\src\HelloWorld.java -f text -R rulesets/java/quickstart.xml -r ..\..\output.txt
 
 echo Error code is %errorlevel%
+
 rem dont display this
 pause
--- StaticCodeAnalyzer/StaticCodeAnalyzer.cpp
+++ StaticCodeAnalyzer/StaticCodeAnalyzer.cpp
@@ -1,10 +1,34 @@
 // StaticCodeAnalyzer.cpp : Defines the entry point for the console application.
 //
-
 #include "stdafx.h"
+#include<fstream>
+#include<iostream>
+#include<string>
+#include<sstream>
 
+using namespace std;
 
 int main()
 {
+	char data[100];
+	string file1 = "..\\ReportFile.emacs";
+	string file2 = "..\\output.txt";
+	ifstream infile1, infile2;
+	infile1.open(file1);
+	infile2.open(file2);
+	while(infile1)
+	{
+
+	infile1 >> data;
+	cout << data;
+		
+	}
+	cout << endl;
+	infile2 >> data;
+	cout << data<<endl;
+
+
+	
+
     return 0;
 }
