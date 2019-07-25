9fdef637edbdce73fad4e168d8a02ca0b490778a
shubhank.joshi <shubhankjjoshi10@gmail.com>
2019-07-25 10:46:12 +0530

    m

--- AutoBuildTest.bat
+++ AutoBuildTest.bat
@@ -1,16 +1,17 @@
-@echo off
+rem @echo off
 
-cd spotbugs-3.1.12\bin
-spotbugs.bat -textui -maxHeap 1500 -nested:false -output ..\..\ReportFile.emacs -effort:max -low -sortByClass -emacs sourcepath ..\..\HelloWorld
+cd pmd-bin-6.16.0\bin
+call pmd.bat -d ..\..\HelloWorld\src\HelloWorld.java -f text -R rulesets\java\quickstart.xml -r ..\..\output.txt
 
-cd ..\..\pmd-bin-6.16.0\bin
- .\pmd.bat -d ..\..\HelloWorld\src\HelloWorld.java -f text -R rulesets/java/quickstart.xml -r ..\..\output.txt
+cd ..\..\spotbugs-3.1.12\bin
+call spotbugs.bat -textui -maxHeap 1500 -nested:false -output ..\..\ReportFile.emacs -effort:max -low -sortByClass -emacs sourcepath ..\..\HelloWorld
 
 cd ..\..\StaticCodeAnalyzer
 g++ StaticCodeAnalyzer.cpp -o StaticCodeAnalyzer.exe
 StaticCodeAnalyzer.exe
 
 echo Error code is %errorlevel%
+
 
 rem dont display this
 pause
